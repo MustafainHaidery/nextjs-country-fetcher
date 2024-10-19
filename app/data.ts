0c1b7898087ccb78cc59e54db316926de06a6d82
[MustafainHaidery] <[mmustafainhaidery@gmail.com]>
2024-10-19 09:14:12 +0500

    project publish

---
+++ app/data.ts
@@ -0,0 +1,28 @@
+
+export const countriesdata = [
+    {
+        name: "pakistan",
+        population: "2200000",
+        capital: "islamabad"
+    },
+    {
+        name: "india",
+        population: "1380004385",
+        capital: "new delhi"
+    },
+    {
+        name: "china",
+        population: "1439323776",
+        capital: "beijing"
+    },
+    {
+        name: "japan",
+        population: "128450000",
+        capital: "tokyo"
+    },
+    {
+        name: "south-korea",
+        population: "51269185",
+        capital: "seoul"
+    }
+]
